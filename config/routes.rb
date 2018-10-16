e91cf75ff69633de09f7596357abfbeb2d351226
Garrett Chesnut <garrettdc2@gmail.com>
2018-10-16 21:43:04 +0000

    Added contacts page and set it up to send emails

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
  root to: 'pages#home'
  
  get 'about', to: 'pages#about'
+ resources :contacts, only: :create
+ get 'contact', to: 'contacts#new', as: 'new_contact'
 end
